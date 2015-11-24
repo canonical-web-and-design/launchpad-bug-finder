09a9489c885b2f95b9e9d8d3f2b128bb506e0f04
Robin Winslow <robin@robinwinslow.co.uk>
2015-11-24 10:46:13 +0000

    Changes to conform to PEP8
    Python is an unusual language in that there is a strong consensus regarding
    best practice. This can be seen right down to the basic language syntax,
    producing a syntax error for indentation errors.
    
    [PEP8][] is the most famous of the "Python Enhancement Guides" (PEPs). It is a
    set of suggestions for code style, with rules governing indentation, spacing,
    naming. The other frequently mentioned PEP is [PEP257][].
    
    There are PEP8 "linters" [for Atom][], [for Sublime][] and most other code
    editors.
    
    I've made fixes to the code to conform with the following PEP8 rules:
    
    - [Indentation][]: "Use 4 spaces per indentation level" (see also [Tabs or Spaces][])
    - [Maximum Line Length][]: "Limit all lines to a maximum of 79 characters"
    - [Blank Lines][]: "Surround top-level function and class definitions with two blank lines" -
      I added an extra line above the _UTF8 class
    - [Imports][]: "Imports are always put at the top of the file" -
      I moved one `import argparse` to the top of the file
    - [Whitespace in Expressions and Statements][]: "Avoid extraneous whitespace" & "Always surround (commas) with a single space on either side" -
      I removed extra whitespace around `+`, added it around `=` and added a space after some commas (`, `).
    
    [PEP8]: https://www.python.org/dev/peps/pep-0008/ "PEP 0008 -- Style Guide for Python Code"
    [PEP257]: https://www.python.org/dev/peps/pep-0257/ "PEP 0257 -- Docstring Conventions"
    [for Atom]: https://atom.io/packages/linter-pep8 "Atom: Linter plugin for pep8"
    [for Sublime]: https://github.com/SublimeLinter/SublimeLinter-pep8 "SublimeLinter plugin for python, using pep8."
    [Indentation]: https://www.python.org/dev/peps/pep-0008/#indentation
    [Tabs or Spaces]: https://www.python.org/dev/peps/pep-0008/#tabs-or-spaces
    [Maximum Line Length]: https://www.python.org/dev/peps/pep-0008/#maximum-line-length
    [Blank Lines]: https://www.python.org/dev/peps/pep-0008/#blank-lines
    [Imports]: https://www.python.org/dev/peps/pep-0008/#imports
    [Whitespace in Expressions and Statements]: https://www.python.org/dev/peps/pep-0008/#whitespace-in-expressions-and-statements

--- UXTeamBugs.py
+++ UXTeamBugs.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 
+import argparse
 from datetime import datetime, timedelta, tzinfo
 from launchpadlib.launchpad import Launchpad
 import sys
 
+
 class _UTC(tzinfo):
     def utcoffset(self, dt):
         return timedelta(0)
-    
+
     def tzname(self, dt):
         return 'UTC'
 
@@ -16,13 +18,18 @@ class _UTC(tzinfo):
 
 UTC = _UTC()
 
-fixed=['Fix Released', 'Fix Committed']
-invalid=['Incomplete', 'Invalid', 'Won\'t Fix', 'Opinion']
-new=['New', 'Triaged', 'Confirmed']
-allStatus=['Fix Released', 'Fix Committed','Incomplete', 'Invalid', 'Won\'t Fix', 'Opinion','New', 'Triaged', 'Confirmed']
+fixed = ['Fix Released', 'Fix Committed']
+invalid = ['Incomplete', 'Invalid', 'Won\'t Fix', 'Opinion']
+new = ['New', 'Triaged', 'Confirmed']
+allStatus = [
+    'Fix Released', 'Fix Committed', 'Incomplete', 'Invalid',
+    'Won\'t Fix', 'Opinion', 'New', 'Triaged', 'Confirmed'
+]
 
-import argparse
-parser = argparse.ArgumentParser('Generate a report of bugs closed in a date range')
+
+parser = argparse.ArgumentParser(
+    'Generate a report of bugs closed in a date range'
+)
 parser.add_argument('start', metavar='START', type=str,
                     help='Start date of the form YYYY-MM-DD')
 parser.add_argument('end', metavar='END', type=str,
@@ -55,51 +62,57 @@ membercount = 0
 print 'Bugs fixed between %s and %s:' % (formatted_start, formatted_end)
 
 for proj_name in proj_list:
-	print
-	print 'Project: ' + proj_name
-	project = launchpad.projects[proj_name]
-	bugTasks = project.searchTasks(status=allStatus)
-	print "Total number of bugs: " + str(len(bugTasks))
-	
-	bugs = [bug for bug in project.searchTasks(status="New")
-		 if bug.date_created and end >= bug.date_created >= start]
-	print 'New bugs:'
-	if len(bugs) > 0:
-	    for bug in bugs:
-		count += 1
-		print bug.title.encode('utf-8').strip()
-	    print 'Total: ' + str(count)
-	    grandtotal += count
-	    count = 0
-	else:
-	    print 'Total: ' + str(count)
+    print
+    print 'Project: ' + proj_name
+    project = launchpad.projects[proj_name]
+    bugTasks = project.searchTasks(status=allStatus)
+    print "Total number of bugs: " + str(len(bugTasks))
 
-	bugs = [bug for bug in project.searchTasks(status=fixed)
-		 if bug.date_closed and end >= bug.date_closed >= start]
-	print 'Bugs fixed'
-	if len(bugs) > 0:
-	    for bug in bugs:
-		count += 1
-		print bug.title.encode('utf-8').strip()
-	    print 'Total: ' + str(count)
-	    grandtotal += count
-	    count = 0
-	else:
-	    print 'Total: ' + str(count)
+    bugs = [
+        bug for bug in project.searchTasks(status="New")
+        if bug.date_created and end >= bug.date_created >= start
+    ]
+    print 'New bugs:'
+    if len(bugs) > 0:
+        for bug in bugs:
+            count += 1
+            print bug.title.encode('utf-8').strip()
+        print 'Total: ' + str(count)
+        grandtotal += count
+        count = 0
+    else:
+        print 'Total: ' + str(count)
 
-	bugs = [bug for bug in project.searchTasks(status=invalid)
-		 if bug.date_closed and end >= bug.date_closed >= start]
-	print
-	print 'Bugs marked as invalid'
-	if len(bugs) > 0:
-	    for bug in bugs:
-		count += 1
-		print bug.title.encode('utf-8').strip()
-	    print 'Total: ' + str(count)
-	    grandtotal += count
-	    count = 0
-	else:
-	    print 'Total: ' + str(count)
+    bugs = [
+        bug for bug in project.searchTasks(status=fixed)
+        if bug.date_closed and end >= bug.date_closed >= start
+    ]
+    print 'Bugs fixed'
+    if len(bugs) > 0:
+        for bug in bugs:
+            count += 1
+            print bug.title.encode('utf-8').strip()
+        print 'Total: ' + str(count)
+        grandtotal += count
+        count = 0
+    else:
+        print 'Total: ' + str(count)
+
+    bugs = [
+        bug for bug in project.searchTasks(status=invalid)
+        if bug.date_closed and end >= bug.date_closed >= start
+    ]
+    print
+    print 'Bugs marked as invalid'
+    if len(bugs) > 0:
+        for bug in bugs:
+            count += 1
+            print bug.title.encode('utf-8').strip()
+        print 'Total: ' + str(count)
+        grandtotal += count
+        count = 0
+    else:
+        print 'Total: ' + str(count)
 print
 print 'Grand Total: ' + str(grandtotal)
 
@@ -111,21 +124,28 @@ members = proj_group.members_details
 
 if len(members) > 0:
     for person in members:
-		membercount += 1
-		memberBugs = project.searchTasks(assignee=person.member,status=allStatus)
-		memberBugsByRange = [bug for bug in memberBugs 
-				      if bug.date_assigned and end >= bug.date_assigned >= start]
-		fixedBugs= project.searchTasks(assignee=person.member,status=fixed)
-		fixedBugsByRange =[bug for bug in fixedBugs 
-				      if bug.date_closed and end >= bug.date_closed >= start]
-		newBugs=project.searchTasks(assignee=person.member,status=new)
-		newBugsByRange =[bug for bug in newBugs 
-				      if bug.date_assigned and end >= bug.date_assigned >= start]
+        membercount += 1
+        memberBugs = project.searchTasks(
+            assignee=person.member,
+            status=allStatus
+        )
+        memberBugsByRange = [
+            bug for bug in memberBugs
+            if bug.date_assigned and end >= bug.date_assigned >= start
+        ]
+        fixedBugs = project.searchTasks(assignee=person.member, status=fixed)
+        fixedBugsByRange = [
+            bug for bug in fixedBugs
+            if bug.date_closed and end >= bug.date_closed >= start
+        ]
+        newBugs = project.searchTasks(assignee=person.member, status=new)
+        newBugsByRange = [
+            bug for bug in newBugs
+            if bug.date_assigned and end >= bug.date_assigned >= start
+        ]
 
-		print person.member.display_name + ' Total: '  +  str(len(memberBugsByRange)) + ' New: '  +  str(len(newBugsByRange)) + ' Fixed: '  +  str(len(fixedBugsByRange))
-		
-
-
-
-
-
+        print (
+            person.member.display_name + ' Total: '
+            + str(len(memberBugsByRange)) + ' New: ' + str(len(newBugsByRange))
+            + ' Fixed: ' + str(len(fixedBugsByRange))
+        )
